e88eb4a7b5093b4092646a569298ee0e943602c7
Anton <asvirkul@gmail.com>
2025-03-11 02:20:36 +0100

    animation added

--- js/script.js
+++ js/script.js
@@ -6,10 +6,10 @@ $(document).ready( function() {
     }).addTo(map);
     
     let customIcon = L.icon({
-        iconUrl: 'images/maps-and-flags.png', // Ссылка на вашу иконку
-        iconSize: [40, 40], // Размер иконки
-        iconAnchor: [20, 40], // Точка привязки (центр низа)
-        popupAnchor: [0, -40] // Сдвиг текста вверх
+        iconUrl: 'images/maps-and-flags.png', 
+        iconSize: [40, 40], 
+        iconAnchor: [20, 40], 
+        popupAnchor: [0, -40] 
     });
     L.marker([51.48051616771504, 31.253714682208884], { icon: customIcon })
     .addTo(map)
@@ -47,5 +47,79 @@ $(document).ready( function() {
             },
         },
     });
+
+    gsap.utils.toArray(".branch").forEach((leaf) => {
+        gsap.to(leaf, {
+            rotation: gsap.utils.random(-5, 5), 
+            x: gsap.utils.random(-2, 2),       
+            yoyo: true,
+            repeat: -1,
+            duration: gsap.utils.random(1.5, 3), 
+            ease: "sine.inOut",
+            delay: gsap.utils.random(0, 1) 
+        });
+    });
+    
+    document.addEventListener("mousemove", (event) => {
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+    
+        document.querySelectorAll(".footer-animation-object").forEach((element) => {
+            const { left, top, width, height } = element.getBoundingClientRect();
+            const centerX = left + width / 2;
+            const centerY = top + height / 2;
+    
+            
+            const deltaX = mouseX - centerX;
+            const deltaY = mouseY - centerY;
+            const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+    
+            
+            const radius = 350;
+    
+            if (distance < radius) {
+
+                const strength = (radius - distance) / radius;
+                const moveX = (deltaX / distance) * strength * 10; 
+                const moveY = (deltaY / distance) * strength * 10;
+    
+                gsap.to(element, {
+                    x: -moveX,
+                    y: -moveY,
+                    rotation: moveX * 2, 
+                    duration: 0.3,
+                    ease: "power2.out",
+                });
+            } else {
+                
+                gsap.to(element, {
+                    x: 0,
+                    y: 0,
+                    rotation: 0,
+                    duration: 1.2,
+                    ease: "elastic.out(1, 0.3)",
+                });
+            }
+        });
+    });
+
+        gsap.from(".hero-map-wrapper > img", {
+            x: -200,
+            opacity: 0,
+            duration: 1.2,
+            ease: "power3.out",
+            stagger: 0.3
+        });
+    
+        gsap.from(".img-hero-text", {
+            x: 100, 
+            opacity: 0,
+            duration: 1.2,
+            ease: "power3.out",
+            stagger: 0.3
+        });
+        
+    
+    
 
 })
