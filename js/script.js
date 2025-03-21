cdafdfbc6ceffcac97743ed247892372764fdc6c
Anton <asvirkul@gmail.com>
2025-03-21 08:15:06 +0100

    pricing page added

--- js/script.js
+++ js/script.js
@@ -113,22 +113,53 @@ $(document).ready( function() {
     
         
 
-            const menuBtn = document.querySelector(".header-mobile-menu");
-            const menu = document.querySelector(".mobile-menu");
+        const menuBtn = document.querySelector(".hamburger");
+        const menu = document.querySelector(".mobile-menu"); 
+        const closeBtn = document.querySelector(".close-menu");
         
-            menuBtn.addEventListener("click", function () {
-                menuBtn.classList.toggle("active");
-                menu.classList.toggle("active");
-            });
+        menuBtn.addEventListener("click", function (e) {
+            e.preventDefault();
+            menu.classList.toggle("active");
+        });
         
-            // Закрытие меню при клике на ссылку
-            document.querySelectorAll(".mobile-menu a").forEach(link => {
-                link.addEventListener("click", function () {
-                    menuBtn.classList.remove("active");
-                    menu.classList.remove("active");
-                });
-            });
-
+        closeBtn.addEventListener("click", function () {
+            menu.classList.remove("active");
+        });
+        
+        const header = document.querySelector("header");
+        let lastScrollTop = 0;
+        let isHeaderVisible = false;
+        
+        window.addEventListener("scroll", function () {
+            let scrollTop = window.scrollY || document.documentElement.scrollTop;
+        
+            if (scrollTop > lastScrollTop) {
+                
+                if (isHeaderVisible) {
+                    gsap.to(header, {
+                        y: "-100%",
+                        opacity: 0,
+                        duration: 0.15, 
+                        ease: "power4.in" 
+                    });
+                    isHeaderVisible = false;
+                }
+            } else {
+              
+                if (!isHeaderVisible) {
+                    gsap.to(header, {
+                        y: "0%",
+                        opacity: 1,
+                        duration: 0.3, 
+                        ease: "power4.out" 
+                    });
+                    isHeaderVisible = true;
+                }
+            }
+        
+            lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; 
+        });
+        
         
 
 })
