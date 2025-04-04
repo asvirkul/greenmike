756aae0bbf44c232edf45080105db0ad0b623c5e
Anton <asvirkul@gmail.com>
2025-04-04 17:24:25 +0200

    team page counter added

--- js/teamPage.js
+++ js/teamPage.js
@@ -10,3 +10,22 @@ const swiper = new Swiper('.swiper', {
     prevEl: '.swiper-button-prev',
   },
 });
+const paragraphs = document.querySelectorAll('.achieve-counter p');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      const delay = +entry.target.dataset.delay;
+      setTimeout(() => {
+        entry.target.classList.remove('hidden');
+      }, delay * 150); // задержка по ступеням
+    }
+  });
+}, {
+  threshold: 0.5
+});
+
+paragraphs.forEach(p => {
+  p.classList.add('hidden');
+  observer.observe(p);
+});
